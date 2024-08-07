fa372aaea190a7b58c9853b3ea891bced1d58152
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-08-07 20:59:19 +0530

    Update averageElements_tuple.py

--- LAB_ASSIGNMENTS/DAY 2 (Tuples)/averageElements_tuple.py
+++ LAB_ASSIGNMENTS/DAY 2 (Tuples)/averageElements_tuple.py
@@ -1,12 +1,7 @@
 def averages_from_tuples(nums):
-    # Unzip the tuples into separate lists
     col1, col2, col3 = zip(*nums)
-    
-    # Calculate the averages using list comprehension
     averages = [sum(col) / len(nums) for col in (col1, col2, col3)]
-    
     return averages
-
 
 nums = ((1, 1, -5), (30, -15, 56), (81, -60, -39), (-10, 2, 3))
 result = averages_from_tuples(nums)

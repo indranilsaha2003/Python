ab052d2edb6fae6d70bb2d7ef70987d79fee64a0
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-08-07 20:36:56 +0530

    Delete Lab_Assignments/listComprehension.py

--- Lab_Assignments/listComprehension.py
+++
@@ -1,8 +0,0 @@
-lst_tuples = [(1, 2), (2, 3, 5), (3, 4), (2, 3, 4, 2)]
-
-
-# Convert each tuple to a list using list comprehension
-lst_lists = [list(tup) for tup in lst_tuples]
-
-
-print(lst_lists)
